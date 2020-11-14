eb0b4dee25c41afbd41e91edff3bbb21c771d5af
vagrant <vagrant@vagrant.vm>
2020-11-14 01:21:42 +0000

    episode_list.html complete and functional

--- static/map_search.js
+++ static/map_search.js
@@ -1,46 +1,64 @@
 "use strict";
-// need to get user selected data (probably just location_id) so we know which pins to
-// send and what api call should be
-// do we call api before rendering map? then send that data through initMap?
 
 
+let map;
 
-//     const locationInfo = new google.maps.InfoWindow();
+function initMap() {
+  map = new google.maps.Map(document.getElementById('searched_map'), {
+    center: { lat: 51.509865, lng: -0.118092 },
+    zoom: 5,
+  });
 
-//     $.get('/episodes.json', (episodes => {
-//         for (const episode of episodes) {
+  locations.forEach(location => {
+    const address = location.address;
+    const latLng = {'lat': Number(location.latitude), 'lng': Number(location.longitude)};
+    const location_ep_id = location.ep_id;
+    const location_id = location.location_id;
+    
+    const locationMarker = new google.maps.Marker({
+        position: latLng,
+        location_ep_id: location_ep_id,
+        location_id: location_id,
+        map: map,
+        icon: {
+                url: "/static/img/tardis.png",
+                size:new google.maps.Size(20, 34)}
+    });
+    const locationInfo = new google.maps.InfoWindow();
+      $.get('/episodes.json', (episodes => {
+        for (const episode of episodes) {
          
-          // const locationInfoContent = (`
-          // <div class="window-content">
-          // // <div class="tardis-thumbnail">
-          // //   <img
-          // //     src="/static/img/tardis.png"
-          // //     alt="tardis"
-          // //   />
-          // // </div>
+          const ep_id = episode.ep_id;
+          locationMarker.get('location_ep_id');
+          const locationInfoContent = (
+          <div class="window-content">
+              <ul class="location-info">
+                <li><b>Season</b>${episode.season}</li>
+                <li><b>Episode Number</b>${episode.episode_number}</li>
+                <li><b>Doctor</b>${episode.doctor}</li>
+                <li><b>Title</b>${episode.title}</li>
+                <li><b>IMDB Link</b>${episode.imdb}</li>
+              </ul>
+          </div>
+          ); 
+          // TODO: need to connect info box ep_id to marker ep_id
+          locationMarker.addEventListener('mouseover', () => {
+            locationInfo.close();
+            locationInfo.setContent(locationInfoContent);
+            locationInfo.open(map, locationMarker);
+          });
+        }
+      )}
+          // need a mouseout event listener to close infowindow
 
-          //     <ul class="location-info">
-          //       <li><b>Season</b>${Episode.season}</li>
-          //       <li><b>Episode Number</b>${Episode.episode_number}</li>
-          //       <li><b>Doctor</b>${Episode.doctor}</li>
-          //       <li><b>Title</b>${Episode.title}</li>
-          //       <li><b>IMDB Link</b>${Episode.imdb}</li>
-          //     </ul>
-          // `);
-    
-    
-          // locationMarker.addEventListener('mouseover', () => {
-          //   locationInfo.close();
-          //   locationInfo.setContent(locationInfoContent);
-          //   locationInfo.open(map, locationMarker);
-    //       // });
-    //     }
-    //   }).fail(() => {
-    //     alert((`
-    //      There are no pins matching your search.
-    //     `));
-    //   });
-    // }
+          locationMarker.addEventListener('click', () => {
+// this needs to send lat/long and ep_id to 
+          })
+
+  };
+} 
+
+
  
     // this should work on click to take you to route , but how to send location_id
     //  .on event delegation - takes one or more events
@@ -48,5 +66,3 @@
     // can use $.ajax - can set converters (will convert json to jquery parseJson)
     // data, method = GET, or PUT?, url (where request is sent), 
     // could do XMLHttpRequest
-
-    
--- static/single_map.js
+++ static/single_map.js
@@ -3,33 +3,39 @@
 
 // Build a separate/permanent info box on single pin map (2nd sprint)
 
+const address = location.address;
+const latLng = {'lat': Number(location.latitude), 'lng': Number(location.longitude)};
+const location_ep_id = location.ep_id;
+
+// Don't want to have an Info box, this will be in a separate window on the right side
+// filter based on location_ep_id
+$.get('/episodes.json', (episodes) => {
+  for (const episode of episodes) {
+    // Define the content of the infoWindow
+        <ul class="episode-info">
+          <li><b>Season</b>${Episode.season}</li>
+          <li><b>Episode Number</b>${Episode.episode_number}</li>
+          <li><b>Doctor</b>${Episode.doctor}</li>
+          <li><b>Title</b>${Episode.title}</li>
+          <li><b>IMDB Link</b>${Episode.imdb}</li>
+        </ul>
+
+let map;
+
 function initMap() {
-    const map = new google.maps.Map(document.getElementById('#single_map') {
+    const map = new google.maps.Map(document.getElementById('#single_map'), {
       center: {
-        lat: 51.509865,
-        lng: -0.118092
-      },
-
-      zoom: 5,
-  
-    //   styles: MAPSTYLES,  // mapStyles is defined in mapstyles.js
-      mapTypeId: google.maps.MapTypeId.TERRAIN
+        latLng },
+      zoom: 10,
     });
-  
-
-    $.get('/episodes.json', (episodes) => {
-        for (const episode of episodes) {
-          // Define the content of the infoWindow
-          const locationInfoContent = (
-              <ul class="location-info">
-                <li><b>Season</b>${Episode.season}</li>
-                <li><b>Episode Number</b>${Episode.episode_number}</li>
-                <li><b>Doctor</b>${Episode.doctor}</li>
-                <li><b>Title</b>${Episode.title}</li>
-                <li><b>IMDB Link</b>${Episode.imdb}</li>
-              </ul>
-          );
-    
+    const locationMarker = new google.maps.Marker({
+      position: latLng,
+      location_ep_id: location_ep_id,
+      map: map,
+      icon: {
+              url: "/static/img/tardis.png",
+              size:new google.maps.Size(20, 34)}
+  });
           const locationMarker = new google.maps.Marker({
             position: {
               lat: Location.latitude,
@@ -37,13 +43,4 @@ function initMap() {
             },
             map: map,
           });
-    
-          locationMarker.addEventListener('mouseover', () => {
-            locationInfo.close();
-            locationInfo.setContent(locationInfoContent);
-            locationInfo.open(map, locationMarker);
-          });
         }
-      });
-    
-
